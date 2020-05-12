7e210dc06b7bbd23c165314fe607a24f5b804994
vd89 <me@daxcodes.in>
2020-05-12 08:55:21 +0530

    the alphabet generator

--- propString.js
+++ propString.js
@@ -10,3 +10,35 @@ let lenMyString = myString.length
 
 console.log(lenMyString); // 9
 console.log(myString); // bluebells
+
+// Methods 
+
+// String.formCharCode()
+// converts ascii dec code to letters 
+
+console.log(String.fromCharCode(65, 90, 78));  // AZN
+console.log(String.fromCharCode(106, 97, 109, 101, 115)); // james
+console.log(String.fromCharCode(100, 97, 120)); // dax 
+
+let findLetter = () => {
+    let strObj =[]
+    for (var i =97; i<= 122; i--) {
+       strObj = String.fromCharCode(i)        
+    }
+    return strObj
+}
+
+console.log(findLetter());
+
+function genAlphabetic() {
+    let alphabet = []
+    for (let i = 97; i <= 122; i++) {
+        let alphabetVal = String.fromCharCode(i)
+        alphabet.push(alphabetVal)
+    }
+    //    return alphabet;
+    console.log(alphabet);
+}
+
+
+genAlphabetic()
